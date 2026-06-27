41022d1f95b4fa7ee33de9a6a7b5a2af3e1a9a81
RealMeddsam <realferidjelalia@gmail.com>
2026-06-27 22:20:05 +0100

    Fix self contained not being self contained (i think ?)

--- Scripts/package/publish-windows-contained.ps1
+++ Scripts/package/publish-windows-contained.ps1
@@ -11,10 +11,12 @@ if (-not (Test-Path -Path "./$BuildDir")) {
 }
 
 $TempPublish = "./$BuildDir/temp-contained"
+$Version = (git describe --tags --abbrev=0).TrimStart('v')
+
 dotnet publish "$Project" /p:PublishProfile=Publish-contained-x64 -c "$Config" -o "$TempPublish"
 
-Copy-Item "$TempPublish/Froststrap.exe" -Destination "./$BuildDir/Froststrap-SelfContained-Setup.exe"
+& makensis /DPUBLISH_DIR="..\$TempPublish" /DAPP_VERSION="$Version" /DSELFCONTAINED=1 Scripts/Installer.nsi
 
 Remove-Item -Recurse -Force "$TempPublish"
 
-Write-Host "Self-contained Windows build complete: $BuildDir/Froststrap-SelfContained-Setup.exe" -ForegroundColor Green
+Write-Host "Self-contained Windows installer complete: $BuildDir/Froststrap-SelfContained-Setup.exe" -ForegroundColor Green
